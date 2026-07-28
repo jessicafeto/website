6d5daa293ebea5eb8b070ab2d8c41a50b7a8abea
XhesikaaFeto <132908684+jessicafeto@users.noreply.github.com>
2026-07-28 21:29:25 +0100

    Darken footer oxblood to #4a1a1e to match the palette

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
   const t = useTranslations("footer");
 
   return (
-    <footer className="border-t border-white/10 bg-[#800020]">
+    <footer className="border-t border-white/10 bg-[#4a1a1e]">
       <div className="wrap py-24">
         <div className="grid gap-20 lg:grid-cols-[1.6fr_1fr]">
           {/* LEFT */}
